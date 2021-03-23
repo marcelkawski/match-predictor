dfda50490b01e8346e25baf04e5cfb12fc417923
Marcel88888 <marcel.kawski1@gmail.com>
2021-03-23 16:32:19 +0100

    Getting clubs data from API in populate_clubs.py implemented.

---
+++ data_providers/clubs/populate_clubs.py
@@ -0,0 +1,84 @@
+import requests
+from data_providers.api_key import api_key
+from data_providers.exceptions.exceptions import CountryNotFoundInApiError, LeagueNotFoundInApiError, \
+    CurrentSeasonNotFoundInApiError
+
+
+def get_country_id(continent, country, headers):
+    params = (
+        ("continent", continent),
+    )
+    response = requests.get('https://app.sportdataapi.com/api/v1/soccer/countries', headers=headers, params=params)
+    countries = response.json()['data']
+    country_id = None
+    for coun in countries.values():
+        if coun['name'] == country:
+            country_id = coun['country_id']
+            break
+    if country_id is None:
+        raise CountryNotFoundInApiError(country)
+    return country_id
+
+
+def get_league_id(continent, country, league, headers):
+    country_id = get_country_id(continent, country, headers)
+    params = (
+        ("country_id", country_id),
+    )
+    response = requests.get('https://app.sportdataapi.com/api/v1/soccer/leagues', headers=headers, params=params)
+    leagues = response.json()['data']
+    league_id = None
+    for leag in leagues.values():
+        if leag['country_id'] == country_id and leag['name'] == league:
+            league_id = leag['league_id']
+    if league_id is None:
+        raise LeagueNotFoundInApiError(country, league)
+    return league_id
+
+
+def get_current_season_id(continent, country, league, headers):
+    league_id = get_league_id(continent, country, league, headers)
+    params = (
+        ("league_id", league_id),
+    )
+    response = requests.get('https://app.sportdataapi.com/api/v1/soccer/seasons', headers=headers, params=params)
+    seasons = response.json()['data']
+    season_id = None
+    for season in seasons:
+        if season['is_current'] == 1:
+            season_id = season['season_id']
+    if season_id is None:
+        raise CurrentSeasonNotFoundInApiError(country, league)
+    return season_id
+
+
+def get_current_season_clubs_ids(continent, country, league, headers):
+    current_season_id = get_current_season_id(continent, country, league, headers)
+    params = (
+        ("season_id", current_season_id),
+    )
+    response = requests.get('https://app.sportdataapi.com/api/v1/soccer/standings', headers=headers, params=params)
+    standings = response.json()['data']['standings']
+    clubs_ids = []
+    for standing in standings:
+        clubs_ids.append(standing['team_id'])
+    return clubs_ids
+
+
+def get_league_current_season_clubs(continent, country, league, headers):
+    current_season_clubs_ids = get_current_season_clubs_ids(continent, country, league, headers)
+    clubs = []
+    for club_id in current_season_clubs_ids:
+        response = requests.get(f'https://app.sportdataapi.com/api/v1/soccer/teams/{club_id}', headers=headers)
+        club_info = response.json()['data']
+        clubs.append({
+            'name': club_info['name']
+        })
+    return clubs
+
+
+if __name__ == '__main__':
+    headers = {
+        "apikey": api_key
+    }
+    get_league_current_season_clubs('Europe', 'Spain', 'LaLiga', headers)
---
+++ data_providers/exceptions/exceptions.py
@@ -0,0 +1,23 @@
+class CountryNotFoundInApiError(Exception):
+    def __init__(self, country_name):
+        self.__country_name = country_name
+        self.__message = f'Error: Country "{self.__country_name}" not found in the API.'
+        super().__init__(self.__message)
+
+
+class LeagueNotFoundInApiError(Exception):
+    def __init__(self, country_name, league_name):
+        self.__country_name = country_name
+        self.__league_name = league_name
+        self.__message = f'Error: League "{self.__league_name}" in the country "{self.__country_name}" not found in ' \
+                         f'the API.'
+        super().__init__(self.__message)
+
+
+class CurrentSeasonNotFoundInApiError(Exception):
+    def __init__(self, country_name, league_name):
+        self.__country_name = country_name
+        self.__league_name = league_name
+        self.__message = f'Error: Current season of the "{self.__league_name}" league in the country ' \
+                         f'"{self.__country_name}" not found in the API.'
+        super().__init__(self.__message)
