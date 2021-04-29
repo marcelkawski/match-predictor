66397636b48690802e6d2d60698b4acfa696178e
Marcel88888 <marcel.kawski1@gmail.com>
2021-04-29 18:49:42 +0200

    Refactor Game model for predicting matches

--- games/models.py
+++ games/models.py
@@ -7,9 +7,9 @@ class Game(models.Model):
     home_team = models.ForeignKey(Club, related_name='home_team_games', on_delete=models.CASCADE)
     visiting_team = models.ForeignKey(Club, related_name='visiting_team_games', on_delete=models.CASCADE)
     date = models.DateTimeField()
-    finished = models.BooleanField(default=False)
-    home_team_goals = models.PositiveIntegerField(null=True, blank=True)
-    visiting_team_goals = models.PositiveIntegerField(null=True, blank=True)
+    ht_chances = models.FloatField(null=True, default=None)
+    draw_chances = models.FloatField(null=True, default=None)
+    at_chances = models.FloatField(null=True, default=None)
     season = models.ForeignKey(Season, blank=True, related_name='season_games', on_delete=models.CASCADE)
 
     def __str__(self):
