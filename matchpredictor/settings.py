5a229c22e8dc430a01ab260b20fb588407852ed4
marcelkawski <marcel.kawski1@gmail.com>
2021-06-14 16:17:53 +0200

    Change colors of the application

--- matchpredictor/settings.py
+++ matchpredictor/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_extensions',
     'bootstrap4',
     'allauth',
     'allauth.account',
