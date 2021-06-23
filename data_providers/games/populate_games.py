20a4235fa949560e3649f0290568c931f843cd6c
marcelkawski <marcel.kawski1@gmail.com>
2021-06-23 15:32:59 +0200

    Improve populate_games.py to get clubs for all leagues

--- data_providers/games/populate_games.py
+++ data_providers/games/populate_games.py
@@ -10,9 +10,10 @@ from data_providers.exceptions.exceptions import CurrentSeasonNotFoundError, Mul
 from games.models import Game
 from clubs.models import Club
 from seasons.models import Season
+from data_providers.leagues import leagues
 
 
-def get_next_games(headers, season_id, date_format='%Y-%m-%d', date_from=None):
+def get_next_games(_headers, season_id, date_format='%Y-%m-%d', date_from=None):
     today = datetime.date.today()
     today_str = today.strftime(date_format)
     if date_from is None:
@@ -21,30 +22,30 @@ def get_next_games(headers, season_id, date_format='%Y-%m-%d', date_from=None):
        ("season_id", season_id),
        ("date_from", date_from)
     )
-    response = requests.get('https://app.sportdataapi.com/api/v1/soccer/matches', headers=headers, params=params)
+    response = requests.get('https://app.sportdataapi.com/api/v1/soccer/matches', headers=_headers, params=params)
     matches = response.json()['data']
-    games = []
+    _games = []
     for match in matches:
         game = {
             'home_team': match['home_team']['name'],
             'away_team': match['away_team']['name'],
             'date_time': match['match_start']
         }
-        games.append(game)
-    return games
+        _games.append(game)
+    return _games
 
 
-def get_current_season(league_name):
+def get_current_season(_league_name):
     try:
-        return Season.objects.get(league__name=league_name, is_active=True)
+        return Season.objects.get(league__name=_league_name, is_active=True)
     except Season.DoesNotExist:
-        raise CurrentSeasonNotFoundError(league_name)
+        raise CurrentSeasonNotFoundError(_league_name)
     except Season.MultipleObjectsReturned:
-        raise MultipleCurrentSeasonsError(league_name)
+        raise MultipleCurrentSeasonsError(_league_name)
 
 
-def save_next_games_to_db(games, current_season, date_format='%Y-%m-%d %H:%M:%S'):
-    for game in games:
+def save_next_games_to_db(_games, _current_season, date_format='%Y-%m-%d %H:%M:%S'):
+    for game in _games:
         home_team = Club.objects.get(name=game['home_team'])
         visiting_team = Club.objects.get(name=game['away_team'])
         naive_datetime = datetime.datetime.strptime(game['date_time'], date_format)
@@ -52,11 +53,12 @@ def save_next_games_to_db(games, current_season, date_format='%Y-%m-%d %H:%M:%S'
         _ = Game.objects.get_or_create(home_team=home_team,
                                        visiting_team=visiting_team,
                                        date=aware_datetime,
-                                       season=current_season)[0]
+                                       season=_current_season)[0]
 
 
 if __name__ == "__main__":
-    league_name = 'Premier League'
-    current_season = get_current_season(league_name)
-    games = get_next_games(headers, current_season.api_id)
-    save_next_games_to_db(games, current_season)
+    league_names = [league['league_name'] for league in leagues]
+    for league_name in league_names:
+        current_season = get_current_season(league_name)
+        games = get_next_games(headers, current_season.api_id)
+        save_next_games_to_db(games, current_season)
