9e9f35bb54bf53413ef61f3d326e57c16eb0c846
marcelkawski <marcel.kawski1@gmail.com>
2021-08-31 16:18:46 +0200

    Limit the number of matches displayed on the home page

--- games/views.py
+++ games/views.py
@@ -13,11 +13,15 @@ class ListGameView(generic.ListView):
         context = super().get_context_data(**kwargs)
         leagues_games = {}
         leagues = League.objects.all()
+        today = datetime.date.today()
+        delta = datetime.timedelta(days=30)
+        date_limit = today + delta
         for league in leagues:
             games_gr_by_date = defaultdict(list)
             games = Game.objects.filter(season__is_active=True,
                                         season__league=league,
-                                        date__gte=datetime.date.today()).order_by('date')
+                                        date__gte=today,
+                                        date__lte=date_limit).order_by('date')
             for game in games:
                 games_gr_by_date[game.date.date()].append(game)
             leagues_games[league] = dict(games_gr_by_date)
